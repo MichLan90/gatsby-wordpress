2e724f74ce5cd5f266b791ac8fef0679b94bc1ed
MichLan90 <michelle.lanaro@medieinstitutet.se>
2020-09-23 14:46:38 +0200

    Modified README file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `My first Gatsby-Wordpress`,
-    description: `From Tim Smith Tutorial.`,
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this 
+    default starter. This barebones starter ships with the main Gatsby 
+    configuration files you might need.`,
     author: `@gatsbyjs`,
   },
   plugins: [
